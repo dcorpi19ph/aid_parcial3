e82e0b267a74d66faf5a7346f6b40003ce1ffa49
dcorpi19ph <dcorpi16010@plataforma-utslp.net>
2024-03-09 10:43:17 -0600

    CORPI ZAVALA ACT1 16010
    09MAR2024 10:43

--- act1_aid.py
+++ act1_aid.py
@@ -6,9 +6,11 @@ sistemaos = sys.platform
 sistema = platform.system()
 version = platform.win32_ver()
 procesador = platform.processor()
+hostname = platform.node()
 
 print("Estamos en {}".format(sistema), " en version: {}".format(version))
 print("Procesador: {}".format(procesador))
+print("Hostname: {}".format(hostname))
 print ("Tipo SO: {}".format(sistemaos))
 if sistema == 'Windows':
     local = subprocess.getoutput("""for /f "tokens=2 delims=[]" %a in ('ping -n 1 -4 "%computername%"') do @echo %a""")

463100620f1b8725e5c159fd5781909a306b1c3c
dcorpi19ph <dcorpi16010@plataforma-utslp.net>
2024-03-09 10:33:00 -0600

    EJERCICIO 1 AID P3
    SO, IP

---
+++ act1_aid.py
@@ -0,0 +1,15 @@
+import platform
+import sys
+import subprocess
+
+sistemaos = sys.platform
+sistema = platform.system()
+version = platform.win32_ver()
+
+print("Estamos en {}".format(sistema), " en version: {}".format(version))
+print ("Tipo SO: {}".format(sistemaos))
+if sistema == 'Windows':
+    local = subprocess.getoutput("""for /f "tokens=2 delims=[]" %a in ('ping -n 1 -4 "%computername%"') do @echo %a""")
+else:
+    local = subprocess.getoutput("ifconfig | grep 'inet ' | grep -Fv 127.0.0.1 | awk '{print $2}'")
+print(local)

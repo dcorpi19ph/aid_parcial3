ff38bae1c305ee59d94b101d91429024a7fbcfbf
dcorpi19ph <dcorpi16010@plataforma-utslp.net>
2024-03-15 18:49:21 -0600

    ACT2
    CORPI ZAVALA ACT2 16010

--- act2_aid.py
+++ act2_aid.py
@@ -1,8 +1,8 @@
 import pywhatkit
 
 try:
-    #pywhatkit.sendwhatmsg("+524445859868", "Hola desde Python", 18, 10)
-    pywhatkit.sendwhatmsg_instantly("+524445859868", "Hola desde Python", 18, 10)
+    pywhatkit.sendwhatmsg_instantly("+524445859868", "Hola desde Python2")
+    #pywhatkit.sendwhatmsg_to_group_instantly("HMSd7sLfFPy4ivP0YtAqvE", "Enviado")
     print("Mensaje enviado")
 except:
     print("ERROR 404 STIC-MÉXICO")

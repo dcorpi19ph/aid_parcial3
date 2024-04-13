b681f1a516e79dbdf79a8e696b25e4a75cbf7408
dcorpi19ph <dcorpi16010@plataforma-utslp.net>
2024-04-13 10:31:32 -0600

    EXAMENP3_CORPI_ZAVALA_DAVID_ALBERTO_IRC9.1_
    EXAMEN PARCIAL 3 AID

---
+++ EXAMEN/ex_p3api.py
@@ -0,0 +1,27 @@
+import platform
+import sys
+import subprocess
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/so', methods=['GET'])
+def obtener_datos_sistema():
+    # Obtener datos del sistema operativo utilizando el módulo 'platform'
+    sistema_operativo = platform.system()
+    version = platform.version()
+    arquitectura = platform.architecture()
+
+    # Crear un diccionario con los datos del sistema operativo
+    datos_sistema = {
+        "sistema_operativo": sistema_operativo,
+        "version": version,
+        "arquitectura": arquitectura
+    }
+
+    # Devolver los datos del sistema operativo como JSON
+    return jsonify(datos_sistema)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
